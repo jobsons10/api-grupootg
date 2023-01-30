635f5643014b25ad063100997f43e95068adeecc
jobsons10 <jobsonsobrinho@grupootg.com>
2023-01-30 01:53:40 -0400

    allow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://www.googleapis.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
